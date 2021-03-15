d14148f2b957af7d35e5571b7c931788ec228b31
pranay213 <45891116+pranay213@users.noreply.github.com>
2021-03-15 15:22:12 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 $dbhost='remotemysql.com';
-$dbuser_name='BSXtjH90Us';
-$dbname='BSXtjH90Us';
-$pass='ECVxo53jAE';
+$dbuser_name='zpkOLsvm5l';
+$dbname='zpkOLsvm5l';
+$pass='gEZJwrYIAv';
 $conn=mysqli_connect($dbhost,$dbuser_name,$pass,$dbname) or die('connectin error');
 
 ?>
