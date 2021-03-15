c90558a2907746d84f5af22231b16003941a906d
pranay213 <45891116+pranay213@users.noreply.github.com>
2021-03-15 15:32:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
-$dbhost='remotemysql.com';
-$dbuser_name='zpkOLsvm5l';
-$dbname='zpkOLsvm5l';
-$pass='gEZJwrYIAv';
+$dbhost='db4free.net';
+$dbuser_name='pranay12';
+$dbname='chatworld';
+$pass='insp@1234';
 $conn=mysqli_connect($dbhost,$dbuser_name,$pass,$dbname) or die('connectin error');
 
 ?>
